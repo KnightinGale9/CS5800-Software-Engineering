461dc82f6f3845332851e1e21dc199fe9f03f294
KnightinGale9 <99992991+KnightinGale9@users.noreply.github.com>
2024-02-08 18:06:23 -0800

    Assignment 1 Adding other files that are for other sections

--- Assignment1/src/Inheritance/HourlyEmployee.java
+++ Assignment1/src/Inheritance/HourlyEmployee.java
@@ -1,4 +1,4 @@
-package inheritance;
+package Inheritance;
 
 public class HourlyEmployee extends Employee{
     protected int wage;
--- Assignment1/src/Inheritance/SalariedEmployee.java
+++ Assignment1/src/Inheritance/SalariedEmployee.java
@@ -1,4 +1,4 @@
-package inheritance;
+package Inheritance;
 
 public class SalariedEmployee extends Employee{
     protected int weeklySalary;
